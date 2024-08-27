2f8eca5aa1289ebe1477d545c0f35c485308ce9c
Bianca Oliveira <169305553+Bima0l@users.noreply.github.com>
2024-08-27 16:00:17 -0300

    Delete JavaScript directory

--- JavaScript/14-08-2024/OLAMUNDO/olamundo.js
+++
@@ -1,8 +0,0 @@
-console.log ("Olá, mundo!")
-
-function somar(a,b){
-return a+b;
-}
-
-var resultado=somar(2,3);
-alert("Resultado:"+resultado);
--- JavaScript/26-08-2024/forwhile.js
+++
@@ -1,9 +0,0 @@
-for(let i=0; i<5; i++){
-    console.log(i);
-}
-
-let contador=0;
-while (contador<3){
-    console.log(contador);
-    contador++;
-}
